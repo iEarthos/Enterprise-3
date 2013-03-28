a96089639e337a2357af06a5ac8492f355fa1ccd
SevenBits <meokid2@yahoo.com>
2013-03-28 16:53:14 -0400

    Working on getting Enterprise to load an EFI program. The effort seems futile, as there is virtually no documentation on this.

---
+++ src/main.h
@@ -0,0 +1,24 @@
+/*
+ * Tool intended to help facilitate the process of booting Linux on Intel Macintosh computers made by Apple from a USB
+ * stick or similar.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * Copyright (C) 2013 SevenBits
+ *
+ */
+
+#ifndef _main_h
+#define _main_h
+
+
+
+#endif
