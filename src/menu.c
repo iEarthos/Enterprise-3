bd769624b3782a0ba2938c7598dabc161c63110e
SevenBits <meokid2@yahoo.com>
2013-10-13 11:44:17 -0400

    Removed duplicate set of #define symbols.

--- src/menu.c
+++ src/menu.c
@@ -155,18 +155,6 @@ static EFI_STATUS key_read(UINT64 *key, BOOLEAN wait) {
 	return 0;
 }
 
-#define KEYPRESS(keys, scan, uni) ((((UINT64)keys) << 32) | ((scan) << 16) | (uni))
-#define EFI_SHIFT_STATE_VALID           0x80000000
-#define EFI_RIGHT_CONTROL_PRESSED       0x00000004
-#define EFI_LEFT_CONTROL_PRESSED        0x00000008
-#define EFI_RIGHT_ALT_PRESSED           0x00000010
-#define EFI_LEFT_ALT_PRESSED            0x00000020
-#define EFI_CONTROL_PRESSED             (EFI_RIGHT_CONTROL_PRESSED|EFI_LEFT_CONTROL_PRESSED)
-#define EFI_ALT_PRESSED                 (EFI_RIGHT_ALT_PRESSED|EFI_LEFT_ALT_PRESSED)
-#define KEYPRESS(keys, scan, uni) ((((UINT64)keys) << 32) | ((scan) << 16) | (uni))
-#define KEYCHAR(k) ((k) & 0xffff)
-#define CHAR_CTRL(c) ((c) - 'a' + 1)
-
 EFI_STATUS display_menu(void) {
 	EFI_STATUS err;
 	UINT64 key;
