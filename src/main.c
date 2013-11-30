61083ed3a4161d2b0b5656f72b5616fc621ffde7
SevenBits <meokid2@yahoo.com>
2013-11-30 09:37:44 -0500

    Freed memory allocated with AllocateZeroPool().

--- src/main.c
+++ src/main.c
@@ -31,7 +31,7 @@ static const EFI_GUID grub_variable_guid = {0x8BE4DF61, 0x93CA, 0x11d2, {0xAA, 0
 #define VERSION_MINOR 1
 
 static LinuxBootOption* ReadConfigurationFile(const CHAR16 *name);
-static CHAR8* KernelLocationForDistributionName(CHAR8 *name, CHAR8 **boot_folder);
+static CHAR8* KernelLocationForDistributionName(CHAR8 *name, OUT CHAR8 **boot_folder);
 static CHAR8* InitRDLocationForDistributionName(CHAR8 *name);
 static EFI_STATUS console_text_mode(VOID);
 
@@ -127,6 +127,8 @@ EFI_STATUS BootLinuxWithOptions(CHAR16 *params) {
 		sizeof(initrd_path[0]) * strlena(initrd_path) + 1, FALSE);
 	efi_set_variable(&grub_variable_guid, L"Enterprise_BootFolder", boot_folder,
 		sizeof(boot_folder[0]) * strlena(boot_folder) + 1, FALSE);
+		
+	FreePool(boot_params); // Free the now-unneeded memory.
 	
 	// Load the EFI boot loader image into memory.
 	path = FileDevicePath(this_image->DeviceHandle, L"\\efi\\boot\\boot.efi");
@@ -185,6 +187,8 @@ static LinuxBootOption* ReadConfigurationFile(const CHAR16 *name) {
 			if (strcmpa((CHAR8 *)"", boot_options->kernel_path) == 0 ||
 				strcmpa((CHAR8 *)"", boot_options->initrd_path) == 0) {
 				Print(L"Distribution family %s is not supported.", ASCIItoUTF16(value, strlena(value)));
+				
+				FreePool(boot_options);
 				return NULL;
 			}
 		// The user is manually specifying information; override any previous values.
@@ -202,7 +206,7 @@ static LinuxBootOption* ReadConfigurationFile(const CHAR16 *name) {
 	return boot_options;
 }
 
-static CHAR8* KernelLocationForDistributionName(CHAR8 *name, CHAR8 **boot_folder) {
+static CHAR8* KernelLocationForDistributionName(CHAR8 *name, OUT CHAR8 **boot_folder) {
 	if (strcmpa((CHAR8 *)"Debian", name) == 0) {
 		*boot_folder = (CHAR8 *)"live";
 		return (CHAR8 *)"/live/vmlinuz";
