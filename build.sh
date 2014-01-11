835aa511bd7eff1ab57c29ebce08fb19cbe3b7c9
SevenBits <meokid2@yahoo.com>
2014-01-11 15:46:08 -0500

    Added a legal notice to build.sh.

--- build.sh
+++ build.sh
@@ -1,4 +1,21 @@
-#!/bin/sh
+#! bin/sh
+#
+# Tool intended to help facilitate the process of booting Linux on Intel
+# Macintosh computers made by Apple from a USB stick or similar.
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation; either version 2.1 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details.
+#
+# Copyright (C) 2013-2014 SevenBits
+#
+#
 make -C src >> /dev/null # Hide regular build messages, we only want errors.
 mkdir bin >> /dev/null 2> /dev/null # Make a new folder if we need to.
 mv src/enterprise.efi bin/enterprise.efi
